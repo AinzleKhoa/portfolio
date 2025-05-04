8fb9b7178b995abeb0b0dcbd8f04ce29bb9fa350
AinzleKhoa <khoale1855@gmail.com>
2025-05-04 16:01:40 +0700

    first test

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function initBackground(headerId) {
+    const header = document.getElementById(headerId);
+    if (!header) return;
+
+    const bg = header.getAttribute('data-bg');
+    if (!bg) return;
+
+    header.style.background = `
+        linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.4) 40%, rgba(0, 0, 0, 0) 100%),
+        url('${bg}')
+    `;
+    header.style.backgroundSize = "cover";
+    header.style.backgroundPosition = `center`;
+    header.style.backgroundRepeat = "no-repeat";
+    header.style.backgroundAttachment = "fixed";
+}
+
+window.onload = function () {
+    initBackground('main-background');
+}
